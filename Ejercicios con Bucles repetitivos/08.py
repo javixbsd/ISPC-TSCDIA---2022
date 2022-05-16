20aec9da622471e312089d7b5a410cfc88975209
javixferdinand <javiersinde@gmail.com>
2022-05-16 02:03:55 -0300

    main

--- Ejercicios con Bucles repetitivos/08.py
+++ Ejercicios con Bucles repetitivos/08.py
@@ -1,13 +1,20 @@
 '''Ejercicio 2.8.
 Escribir un programa que pida al usuario un número entero positivo y muestre por pantalla la cuenta desde atrás de ese número hasta cero separados por comas.'''
 
-from pickle import APPEND
+
+from os import sep
+
 
 
 n = int(input("Ingrese un entero positivo: "))
 
-salida = []
+
 for numero in range(n,0,-1):
-    salida.append(numero)
+    print(numero,end=",") 
+    
   
-print(salida)  
+print()
+
+
+c=int(input("Ingrese un numero:"))
+print(*range(c,0,-1), sep=',')
